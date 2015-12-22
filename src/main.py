a405e262d5b0075d1597cc163c226570d927c9e8
Qurban <qurban.ali@iceanimations.com>
2015-12-22 10:14:09 +0500

    utilities path added

--- src/main.py
+++ src/main.py
@@ -4,16 +4,11 @@ Created on Aug 12, 2015
 @author: qurban.ali
 '''
 import sys
-import os
-if os.environ['USERNAME'] == 'qurban.ali':
-    sys.path.insert(0, 'D:/My/Tasks/workSpace')
-else:
-    sys.path.insert(0, 'R:/Python_Scripts/plugins')
-
+sys.path.insert(0, 'R:/Python_Scripts/plugins')
+sys.path.insert(0, 'R:/Python_Scripts/plugins/utilities')
 import sip
 sip.setapi('QString', 2)
 from PyQt4.QtGui import QApplication, QStyleFactory
-import sys
 import _compositing as comp
 
 
